e5b5f15226698261aa55fe97adaa199ddafce8da
Aman Raj <67687726+raj01Aman@users.noreply.github.com>
2024-08-08 21:44:45 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,5 @@
 <?php
+
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,7 +8,8 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+
+  $receiving_email_address = 'pixusfilms@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,11 +19,21 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
+  $from_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  $from_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+  $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->from_name = $from_name;
+  $contact->from_email = $from_email;
+  $contact->subject = $subject;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +45,19 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
+  $contact->add_message($from_name, 'From');
+  $contact->add_message($from_email, 'Email');
+  $contact->add_message($message, 'Message', 10);
+
+  try {
+    echo $contact->send();
+  } catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+  }
+  
 ?>
