b5b254b91f4cf0fdf0cc18b33a73f24856125994
Aman Raj <67687726+raj01Aman@users.noreply.github.com>
2024-08-08 21:49:07 +0530

    Update contact.php
    kln

--- forms/contact.php
+++ forms/contact.php
@@ -20,20 +20,11 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
 
-  $from_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-  $from_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-  $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-  $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $from_name;
-  $contact->from_email = $from_email;
-  $contact->subject = $subject;
   
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -45,19 +36,10 @@
   );
   */
 
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  // echo $contact->send();
-  $contact->add_message($from_name, 'From');
-  $contact->add_message($from_email, 'Email');
-  $contact->add_message($message, 'Message', 10);
-
-  try {
-    echo $contact->send();
-  } catch (Exception $e) {
-    echo 'Error: ' . $e->getMessage();
-  }
+  echo $contact->send();
   
 ?>
